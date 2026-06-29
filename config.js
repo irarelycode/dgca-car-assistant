79172f19927acbfb24f42656f64bfdae1b902a95
Shubham Bharti <77288703+irarelycode@users.noreply.github.com>
2026-06-30 00:00:01 +0530

    Update SHEET_CSV_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // === DGCA LIVE CONFIG - SECURE VERSION ===
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3UdKh-h0s6gr9ADPQyQH8oHYwK21m3u0H-zqEbHQv6raUnrNk6TPMTiY4-S80ujxZ1KStBJX7fdNd/pub?gid=0&single=true&output=csv";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3UdKh-hOs6gr9ADPQyQH8oHYwK2lm3u0H-zqEbHQv6raUnrNk6TPMTiY4-S80ujxZlKStBJX7fdNd/pub?gid=0&single=true&output=csv";
 const SHEET_VIEW_URL = "https://docs.google.com/spreadsheets/d/1JJCRGWs1HoI2wcAtEZU9HiDKxaFZmoA0Hx-Cu0JlU_E/edit";
 
 // Your Cloudflare Worker URL - replace after you deploy
