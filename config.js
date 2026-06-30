895be0f3461f4f9ec1e57e3359b0c2512dd31363
Shubham Bharti <77288703+irarelycode@users.noreply.github.com>
2026-06-30 10:54:14 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3UdKh-h0s6gr9ADPQyQH8oHYwK21m3u0H-zqEbHQv6raUnrNk6TPMTiY4-S80ujxZ1KStBJX7fdNd/pub?gid=0&single=true&output=csv";
-const SHEET_VIEW_URL = "https://docs.google.com/spreadsheets/d/1JJCRGWs1HoI2wcAtEZU9HiDKxaFZmoA0Hx-Cu0JlU_E/edit";
-const WORKER_URL = "https://dgca-groq-proxy.shubhamkumarvinod.workers.dev";
-// Replace with your Apps Script Web App URL after deployment
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwLWDwFCQcaS5fHVIBFFu7F_cKlnzNWnhcW9nQQyJVJl4rtY4ZUzvSfKlCBbgF8M2697g/exec";
-const GROQ_MODEL = "llama-3.3-70b-versatile";
